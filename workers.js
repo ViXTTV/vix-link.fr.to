2bd74f06fcd07f912f958d666a984eef5653cf83
ViX <104933156+ViXTTV@users.noreply.github.com>
2024-04-21 02:15:07 +0200

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,9 +1,9 @@
 async function handleRequest(request) {
     const userAgent = request.headers.get('User-Agent') || '';
     if (!userAgent.includes('discord')) {
-        return Response.redirect('https://blog.flky.dev', 301);
+        return Response.redirect('https://vix.fr.to', 301);
     } else {
-    return fetch("https://discordredirect.flky.dev/demo.jpeg");
+    return fetch("https://vix-link.fr.to/iwantyoutojointhediscordservervix.jpg");
 }}
   
   addEventListener('fetch', event => {
